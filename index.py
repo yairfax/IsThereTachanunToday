50c7613baaf0913cce84265898e6e1516bedf4f4
Yair <yairshop@gmail.com>
2020-06-07 16:46:14 -0400

    deploy

--- index.py
+++ index.py
@@ -23,4 +23,4 @@ def main():
         hebrew_date="%d %s %d" % (response["hd"], response["hm"], response["hy"]),
         hebrew_date_hebrew=response["hebrew"],
         date_placeholder=today.isoformat(),
-        reason=tachanun_huh["description"] if tachanun_huh else "")
+        reason=tachanun_huh["description"] if tachanun_huh else "") 
