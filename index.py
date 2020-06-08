bf45ab64f344bec7ec1923e9f223b04607f82f4d
Yair <yairshop@gmail.com>
2020-06-08 13:15:40 -0400

    Migrate to pyluach, no foreign API calls

--- index.py
+++ index.py
@@ -2,25 +2,23 @@ from flask import Flask, render_template, request
 from datetime import date
 from tachanun import *
 import requests
-
+from pyluach import dates
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    today = date.today()
+    today_hebrew = dates.HebrewDate.today()
+    today_greg = today_hebrew.to_pydate()
 
-    if 'date' in request.args:
-        today = today.fromisoformat(request.args.get('date'))
+    if 'g_date' in request.args:
+        today_greg = date.fromisoformat(request.args.get('g_date'))
+        today_hebrew = today_hebrew.from_pydate(today_greg)
 
-    r = requests.get('https://www.hebcal.com/converter/?cfg=json&gy=%d&gm=%d&gd=%d&g2h=1' % (today.year, today.month, today.day))
-
-    response = r.json()
-
-    tachanun_huh = no_tachanun(response["hm"], response["hd"])
+    tachanun_huh = no_tachanun(today_hebrew)
     return render_template('main.jinja',
         no_tachanun=tachanun_huh,
-        date=today.strftime("%B %d, %Y"),
-        hebrew_date="%d %s %d" % (response["hd"], response["hm"], response["hy"]),
-        hebrew_date_hebrew=response["hebrew"],
-        date_placeholder=today.isoformat(),
+        date=today_greg.strftime("%B %d, %Y"),
+        hebrew_date="%d %s %d" % (today_hebrew.day, month_str(today_hebrew), today_hebrew.year),
+        hebrew_date_hebrew=get_hebrew_date_str(today_hebrew),
+        date_placeholder=today_greg.isoformat(),
         reason=tachanun_huh["description"] if tachanun_huh else "")
--- tachanun.py
+++ tachanun.py
@@ -1,14 +1,43 @@
 import json
+from pyluach import dates, hebrewcal
+from njdate import gematria
 
+month_map = {
+    "tishrei": "תשרי",
+    "cheshvan": "חשוון",
+    "kislev": "כסלו",
+    "tevet": "טבת",
+    "shvat": "שבט",
+    "adar": "אדר",
+    "nisan": "ניסן",
+    "iyyar": "אייר",
+    "sivan": "סיון",
+    "tamuz": "טמוז",
+    "av": "אב",
+    "elul": "אלול",
+    "adar_i": "אדר א׳",
+    "adar_ii": "אדר ב׳"
+}
 data = json.load(open("tachanun_days.json"))
 
-def no_tachanun(month, day):
-    month = month.lower().replace("'", "").replace(" ii", "2").replace(" i", "1")
-    
+def no_tachanun(date):
+    month = get_key(date)
     days = data[month]
 
-    for day_dict in days:
-        if day >= day_dict["startDay"] and day <= day_dict["endDay"]:
-            return day_dict
+    for day in days:
+        if date.day >= day["startDay"] and date.day <= day["endDay"]:
+            return day
     
     return False
+
+def fix_spelling(date_str):
+    return date_str.replace("Teves", "Tevet").replace("Iyar", "Iyyar").replace("Nissan", "Nisan").replace("Rishon", "Aleph").replace("Sheni", "Bet")
+
+def get_key(date):    
+    return month_str(date).lower().replace("aleph", "_i").replace("bet", "_ii")
+
+def month_str(date):
+    return fix_spelling(hebrewcal.Month(date.year, date.month).name)
+
+def get_hebrew_date_str(date):
+    return "%s %s, %s" % (gematria.NumberToGematria(date.day), month_map[get_key(date)], gematria.YearNoToGematria(date.year, sofit=False))
