cc05a24c7b54eaba302b8c22117ac1c7a61748dd
Yair <yairshop@gmail.com>
2020-06-10 16:49:28 -0400

    add hebrew date selector and backend

--- index.py
+++ index.py
@@ -13,11 +13,18 @@ def main():
     if 'g_date' in request.args:
         today_greg = date.fromisoformat(request.args.get('g_date'))
         today_hebrew = dates.HebrewDate.from_pydate(today_greg)
+    elif 'h_day' in request.args:
+        today_hebrew = dates.HebrewDate(int(request.args.get('h_year')), int(request.args.get('h_month')), int(request.args.get('h_day')))
+        today_greg = today_hebrew.to_pydate()
+
 
     tachanun_huh = no_tachanun(today_hebrew)
     return render_template('main.html',
         no_tachanun="mincha" not in tachanun_huh if tachanun_huh else False,
         date=today_greg.strftime("%B %d, %Y"),
+        h_day=today_hebrew.day,
+        h_month=today_hebrew.month,
+        h_year=today_hebrew.year,
         hebrew_date="%d %s %d" % (today_hebrew.day, month_str(today_hebrew), today_hebrew.year),
         hebrew_date_hebrew=hebrew_date_str(today_hebrew),
         date_placeholder=today_greg.isoformat(),
