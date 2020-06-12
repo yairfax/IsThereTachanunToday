113ec96a8c2088b4512ee7bb30f0f65790659c6b
Yair <yairshop@gmail.com>
2020-06-12 11:24:23 -0400

    implement /il page and change links in date picker to stay on israel

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from pyluach import dates, hebrewcal
 app = Flask(__name__)
 
 @app.route('/')
-def main():
+def main(il=False):
     today_hebrew = dates.HebrewDate.today()
     today_greg = today_hebrew.to_pydate()
 
@@ -36,11 +36,16 @@ def main():
         hebrew_date="%d %s %d" % (today_hebrew.day, month_str(today_hebrew), today_hebrew.year),
         hebrew_date_hebrew=hebrew_date_str(today_hebrew),
         date_placeholder=today_greg.isoformat(),
-        reason=tachanun_huh["description"] if tachanun_huh else "",
+        reason=tachanun_huh["description_il"] if (il and "description_il" in tachanun_huh) else (tachanun_huh["description"] if tachanun_huh else ""),
         source="http://www.sefaria.org/%s" % tachanun_huh["source"] if tachanun_huh else "",
         subtitle=tachanun_huh["subtitle"] if tachanun_huh and "subtitle" in tachanun_huh else "",
         mincha="mincha" in tachanun_huh if tachanun_huh else False,
-        error=error)
+        error=error,
+        il="il" if il else "")
+
+@app.route("/il")
+def il():
+    return main(il=True)
 
 @app.route("/get_months/<int:year>")
 def get_months(year):
